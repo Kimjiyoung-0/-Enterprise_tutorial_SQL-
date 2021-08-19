d456cda30642e447db0b7a1bf988108f7ecd4ea9
kimjiyoung-0 <9331wldyd@daum.net>
2021-08-19 16:33:17 +0900

    11차과제 중간완료

---
+++ PLSQL/11차과제/SQL11차과제선언부_2_김지용.sql
@@ -0,0 +1,27 @@
+create or replace package ddl_get_pkg
+IS 
+
+    procedure tab_proc
+    (
+        f_user varchar2,  -- user 로 부터 입력받을 유저이름
+        f_tabname varchar2 -- user 로 부터 입력받을 테이블이름
+    );
+    
+    PROCEDURE source_proc
+    (
+        f_user varchar2,  -- user 로 부터 입력받을 유저이름
+        f_objname varchar2 -- user 로 부터 입력받을 오브젝트이름    
+    );
+    
+    procedure ind_proc
+    (
+        f_user varchar2,  -- user 로 부터 입력받을 유저이름
+        f_tabname varchar2 -- user 로 부터 입력받을 인덱스이름
+    );
+    procedure view_proc
+    (
+        f_user varchar2,  -- user 로 부터 입력받을 유저이름
+        f_viewname varchar2 -- user 로 부터 입력받을 인덱스이름
+    );
+    
+END ddl_get_pkg;
