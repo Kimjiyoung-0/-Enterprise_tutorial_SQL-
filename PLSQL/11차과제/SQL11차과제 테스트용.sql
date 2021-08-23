e28819efd37859f2700e6dc69288f49fcc2ca031
kimjiyoung-0 <9331wldyd@daum.net>
2021-08-23 17:40:51 +0900

    오류 수정및 테스트파일 최적화

--- PLSQL/11차과제/SQL11차과제 테스트용.sql
+++ PLSQL/11차과제/SQL11차과제 테스트용.sql
@@ -1,23 +1,32 @@
-EXEC ddl_get_pkg.tab_proc('HR','EMP');
+EXEC ddl_get_pkg.tab_proc('HR','EMP');--table
 
-EXEC ddl_get_pkg.source_proc('SYS','schedFileWatcherJava');
+EXEC ddl_get_pkg.ind_proc('HR','EMP');--index
 
-EXEC ddl_get_pkg.ind_proc('HR','EMP');
+EXEC ddl_get_pkg.view_proc('HR','EMP_DETAILS_VIEW');--view
 
-EXEC ddl_get_pkg.view_proc('HR','EMP_DETAILS_VIEW');
+EXEC ddl_get_pkg.seq_proc('HR','DEPARTMENTS_SEQ');--seq
 
+EXEC ddl_get_pkg.source_proc('HR','INDEX_COLUMN_FUNCTION');--function
 
+EXEC ddl_get_pkg.source_proc('HR','DML_LOGS');--tri
+
+EXEC ddl_get_pkg.source_proc('HR','RESTORE_SQL_PROCEDURE');--proc
+
+EXEC ddl_get_pkg.source_proc('SYS','DDL_GET_PKG');--package
 
 select * from ddl_scripts
-order by object_type desc,OBJECT_NAME,LINE ;
+order by object_type,OBJECT_NAME,LINE ;
+
 
 delete  from ddl_scripts;
-
 commit;
 
 --완료 목록
 --table 
 --index 
+--view 
+--SEQUENCE
+
 --PACKAGE BODY 
 --TYPE BODY 
 --TRIGGER
@@ -26,9 +35,3 @@ commit;
 --LIBRARY 
 --FUNCTION 
 --TYPE 
---JAVA SOURCE
---view 
-
---해야되는것들
---SEQ
---
